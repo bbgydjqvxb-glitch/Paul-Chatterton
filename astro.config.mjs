19d86a535f2159c272229118f562a8931b3c0337
bbgydjqvxb-glitch <bbgydjqvxb@privaterelay.appleid.com>
2026-01-30 11:21:49 +0000

    Add Astro configuration for site deployment

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // This tells Astro your website is in a subfolder called /Paul-chatterton/
+  site: 'https://bbgydjqvxb-glitch.github.io',
+  base: 'Paul-chatterton',
+  output: 'static',
+});
